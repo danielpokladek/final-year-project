025b7462894bb5caaf58fab1b763dcc88877d5ff
Daniel Pokladek <danielpokladek@gmail.com>
2019-02-24 01:40:55 +0000

    Ripples are finished (just need minor tweaking)
    Shader graph version of ripples is now done.
    
    Users can now input a base texture, this is the texture that the ripples will be overlayed onto. Users can add a normal texture that supports the base texture to make the effect more appealing and make the light reflect better.
    
    I have changed the way the normals are handled. They are now a blend between the base texture's normal and the ripples normals.
    
    Its been quite a difficult task making the first part of this shader, as there were many differences between Unity and Unreal, but I had to learn a lot of new things to be able to create the shaders.
    
    More in the upcoming blog.

---
+++ Graph-Shaders-DanielPokladek/Assets/Editor/NormalBlendNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+[Title("CustomNode", "NormalBlend")]
+public class NormalBlendNode : CodeFunctionNode
+{
+    public NormalBlendNode()
+    {
+        name = "NormalBlend";
+    }
+
+    protected override MethodInfo GetFunctionToConvert()
+    {
+        return GetType().GetMethod("NormalBlend", BindingFlags.Static | BindingFlags.NonPublic);
+    }
+
+    private static string NormalBlend(
+        [Slot(0, Binding.None)]Vector3 A,
+        [Slot(1, Binding.None)]Vector3 B,
+        [Slot(2, Binding.None)]out Vector1 Out)
+    {
+        return @"
+        {
+            Out = A + B;
+        }";
+    }
+}
